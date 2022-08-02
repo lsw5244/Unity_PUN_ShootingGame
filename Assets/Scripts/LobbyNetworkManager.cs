f51f47a0821a3e238460d8c778b60bc946ba5dea
이성우 <lsw5244@naver.com>
2022-08-02 15:18:34 +0900

    feat : LobbyNetworkManager를 통해 포톤 서버에 접속하도록 구현

---
+++ Assets/Scripts/LobbyNetworkManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class LobbyNetworkManager : MonoBehaviourPunCallbacks
+{
+    string gameVersion = "0.1";
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    // Photon Server에 접속하는 함수
+    void Connect()
+    {
+        Debug.Log("Connect호출 !!");
+        if (PhotonNetwork.IsConnected == true)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            // 해당 버전으로 photon 클라이드로 연결되는 시작점 ( Photon Online Server에 접속하는 함수 )
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("서버에 입장했습니다.");
+        // Lobby에 접속하는 함수
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비에 입장하였습니다.");
+    }
+}
