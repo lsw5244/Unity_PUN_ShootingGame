23896ce5f4b1f6239fe50b52c68d249d5fff6278
이성우 <lsw5244@naver.com>
2022-08-09 15:09:04 +0900

    Refactor : PlayerFire.cs코드 정리

--- Assets/Scripts/Player/PlayerFire.cs
+++ Assets/Scripts/Player/PlayerFire.cs
@@ -23,10 +23,9 @@ public class PlayerFire : MonoBehaviour
 
     void Update()
     {
-        //if(photonView.IsMine == true)
+        if(photonView.IsMine == true)
         {
             mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //Debug.Log(mousePosition);
             GunPivotSetting();
 
             Fire();
@@ -37,7 +36,6 @@ public class PlayerFire : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            //mousePosition = Input.mousePosition;
             GameObject bullet = PhotonNetwork.Instantiate("TempBullet", Vector2.zero, Quaternion.identity);
             bullet.GetComponent<Bullet>().Shoot(mousePosition, state.bulletPower, state.attackDamage);
         }
@@ -45,33 +43,17 @@ public class PlayerFire : MonoBehaviour
 
     void GunPivotSetting()
     {
-        Vector2 v = mousePosition - (Vector2)gunPivot.position;
+        Vector2 toMousePosition = mousePosition - (Vector2)gunPivot.position;
 
-        //float dot = Vector3.Dot(forward, toTarget); // +면 앞, -면 뒤에있는 것
-
-
-        //float dot = Vector3.Dot(forward, toTarget); // +면 앞, -면 뒤에있는 것
-        //float angle = Mathf.Acos(dot / toTarget.magnitude); // 타겟의 각도 구하기, 라디안
-
-        // 왼쪽에 있으면 -, 오른쪽에 있으면 +  (좌 우 판별)
-        //Debug.Log(Vector3.Cross(gunPivot.transform.right, v).y);
-        //float crossY = Vector3.Cross(gunPivot.transform.right, v).x;  // 상, 하 판별
-        //Debug.Log(crossY);
-
-
-        float dot2 = Vector2.Dot(gunPivot.up, v);   // +면 위 -면 아래
-                                                    //Debug.Log(dot2);
-
-        float dot = Vector2.Dot(gunPivot.right, v);     // 상+ 하- 판별
-        float angle = Mathf.Acos(dot / v.magnitude) * Mathf.Rad2Deg;
-        Debug.Log($"dot :{dot}  angle : {angle/* * Mathf.Rad2Deg*/}");
+        float dot = Vector2.Dot(gunPivot.right, toMousePosition);   // 좌- 우+ 확인 가능
+        float angle = Mathf.Acos(dot / toMousePosition.magnitude) * Mathf.Rad2Deg;    // 마우스 포인터와의 각도
 
         if (float.IsNaN(angle) == true)
         {
             return;
         }
 
-        if (Vector2.Dot(gunPivot.up, v) >= 0.0f)
+        if (Vector2.Dot(gunPivot.up, toMousePosition) >= 0.0f)    // +면 위 -면 아래에 마우스 포인터가 존재함
         {
             gunPivot.rotation = gunPivot.rotation * Quaternion.Euler(0f, 0f, angle);
         }
@@ -79,9 +61,5 @@ public class PlayerFire : MonoBehaviour
         {
             gunPivot.rotation = gunPivot.rotation * Quaternion.Euler(0f, 0f, -angle);
         }
-
-
-
-
     }
 }
