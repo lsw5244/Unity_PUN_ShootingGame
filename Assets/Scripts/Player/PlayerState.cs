890baaf544d92bdafffe2ed3263c20495d002fed
이성우 <lsw5244@naver.com>
2022-08-24 20:37:10 +0900

    feat : PlayerState에 HP와 MaxHP로 나누어 HP를 관리하도록 구현

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Photon.Pun;
 
 public class PlayerState : MonoBehaviour, IPunObservable
 {
-    public float HP = 100f;
+    public float maxHP = 100f;
+    public float HP;
     public float moveSpeed = 10f;
     public float jumpPower = 10f;
 
@@ -17,6 +19,9 @@ public class PlayerState : MonoBehaviour, IPunObservable
     public int maxBulletCount = 3;
 
     public float reloadTime = 1.0f;
+
+    [SerializeField]
+    private Image hpBar;
 
     [HideInInspector]
     public PhotonView photonView;
@@ -40,11 +45,17 @@ public class PlayerState : MonoBehaviour, IPunObservable
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
+        HP = maxHP;
     }
 
     public void GetDamage(float Damage)
     {
         Debug.Log($"{gameObject.name}의 GetDamage가 호출되어 데미지를 주었다 !!!");
         HP -= Damage;        
+    }
+
+    void HpBarUpdate()
+    {
+        hpBar.fillAmount = HP / maxHP;
     }
 }
