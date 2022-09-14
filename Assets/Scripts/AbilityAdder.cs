cc5044b2cb7625d795fab00dbe4242b3e2382c62
이성우 <lsw5244@naver.com>
2022-09-14 18:41:04 +0900

    feat : AbilityAdder에서 리플렉션을 통해 함수를 실행하도록 구현
    스페이스바를 누르면 임시로 할당한 메소드를 검색하여 실행하도록 구현

--- Assets/Scripts/AbilityAdder.cs
+++ Assets/Scripts/AbilityAdder.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Reflection;
+
 public class AbilityAdder : MonoBehaviour
 {
     private string[] addAbilityNames
@@ -9,11 +11,25 @@ public class AbilityAdder : MonoBehaviour
 
     private int[] selectAbilityIdxs = new int[3];
 
+    private System.Type type;
+
     private void Start()
     {
         /* 해야할 것 중복없이 숫자 3개 뽑기 */        
         ChooseAbilityIdxs();
     }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            type = this.GetType();
+            Debug.Log(type);
+            MethodInfo mi = type.GetMethod("AddBulletExplosion", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            mi.Invoke(this, null);
+        }
+    }     
 
     void ChooseAbilityIdxs()
     {
@@ -35,27 +51,25 @@ public class AbilityAdder : MonoBehaviour
         {
             Debug.Log($"{i}번째 숫자는 {selectAbilityIdxs[i]}");
         }
-
-        Random.Range(0, addAbilityNames.Length);
     }
-
+    /* ===========아래에는 특성 추가 함수=========== */
     void AddBulletExplosion()
     {
-
+        Debug.Log("AddBulletExplosion 실행 !!!");
     }
 
-    void AddPoisonBullet()
+    public void AddPoisonBullet()
     {
-
+        Debug.Log("AddPoisonBullet 실행 !!!");
     }
 
-    void AddGlassCannon()
+    public void AddGlassCannon()
     {
-
+        Debug.Log("AddGlassCannon 실행 !!!");
     }
 
-    void AddCombine()
+    public void AddCombine()
     {
-
+        Debug.Log("AddCombine 실행 !!!");
     }
 }
