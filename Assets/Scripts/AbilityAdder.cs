dc810f11ced528c1297c81614176e15d2de4a8d7
이성우 <lsw5244@naver.com>
2022-09-19 16:02:01 +0900

    feat : AbilityAdder에서 카드의 이미지와 이름을 출력하도록 구현
    랜덤으로 선택한 특성에 따라 특성 카드의 이미지와 특성의 이름을
    출력시키도록 구현

--- Assets/Scripts/AbilityAdder.cs
+++ Assets/Scripts/AbilityAdder.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using System.Reflection;
 using Photon.Pun;
@@ -9,22 +10,30 @@ public class AbilityAdder : MonoBehaviour
 {
     private string[] addAbilityNames
         = { "AddBulletExplosion", "AddPoisonBullet", "AddGlassCannon", "AddCombine"  };
+    [SerializeField]
+    private Sprite[] abilityImagesResources;
 
-    private int[] selectAbilityIdxs = new int[3];
+    private int[] randomAbilityIdxs = new int[3];
     private int currentSelectAbilityIdx = 0;
 
     private System.Type type;
 
+    [SerializeField]
+    private Image[] abilityImages = new Image[3];
+    [SerializeField]
+    private Text[] abilityNameTexts = new Text[3];
+
     private void Start()
     {
-        /* 중복없이 숫자 3개 뽑기 */        
+        // 마스터가 정하고 분배시키기
+        /* 중복없이 숫자 3개 뽑기 */
         ChooseAbilityIdxs();
 
-        // 리플렉션을 통해 런타임 중 문자열로 함수 실행시키기
-        type = this.GetType();
-        MethodInfo mi = type.GetMethod("AddBulletExplosion", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        //mi.Invoke(this, null);
+        for (int i = 0; i < randomAbilityIdxs.Length; ++i)
+        {
+            abilityImages[i].sprite = abilityImagesResources[randomAbilityIdxs[i]];
+            abilityNameTexts[i].text = addAbilityNames[randomAbilityIdxs[i]].Replace("Add", "");
+        }
     }
 
     private void Update()
@@ -47,50 +56,21 @@ public class AbilityAdder : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                MethodInfo mi = type.GetMethod(addAbilityNames[selectAbilityIdxs[currentSelectAbilityIdx]], BindingFlags.NonPublic | BindingFlags.Instance);
+                MethodInfo mi = type.GetMethod(addAbilityNames[randomAbilityIdxs[currentSelectAbilityIdx]], BindingFlags.NonPublic | BindingFlags.Instance);
                 mi.Invoke(this, null);
             }
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-            Debug.Log(type);
-
-        }
-
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            MethodInfo mi = type.GetMethod(addAbilityNames[0], BindingFlags.NonPublic | BindingFlags.Instance);
-            mi.Invoke(this, null);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            MethodInfo mi = type.GetMethod(addAbilityNames[1], BindingFlags.NonPublic | BindingFlags.Instance);
-            mi.Invoke(this, null);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            MethodInfo mi = type.GetMethod(addAbilityNames[2], BindingFlags.NonPublic | BindingFlags.Instance);
-            mi.Invoke(this, null);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            MethodInfo mi = type.GetMethod(addAbilityNames[3], BindingFlags.NonPublic | BindingFlags.Instance);
-            mi.Invoke(this, null);
         }
     }     
 
     void ChooseAbilityIdxs()
     {
-        for(int i = 0; i < selectAbilityIdxs.Length; ++i)
+        for(int i = 0; i < randomAbilityIdxs.Length; ++i)
         {
-            selectAbilityIdxs[i] = Random.Range(0, addAbilityNames.Length);
+            randomAbilityIdxs[i] = Random.Range(0, addAbilityNames.Length);
             
             for(int j = 0; j < i; j++)
             {
-                if (selectAbilityIdxs[i] == selectAbilityIdxs[j])
+                if (randomAbilityIdxs[i] == randomAbilityIdxs[j])
                 {
                     i--;
                     break;
@@ -98,9 +78,9 @@ public class AbilityAdder : MonoBehaviour
             }
         }
 
-        for(int i = 0; i < selectAbilityIdxs.Length; ++i)
+        for(int i = 0; i < randomAbilityIdxs.Length; ++i)
         {
-            Debug.Log($"{i}번째 숫자는 {selectAbilityIdxs[i]}");
+            Debug.Log($"{i}번째 숫자는 {randomAbilityIdxs[i]}");
         }
     }
     /* ===========아래에는 특성 추가 함수=========== */
